34b2614641bfc6693bad88dfe05981c00c714e0d
abel abraha <abrahaabel18@gmail.com>
2024-03-20 17:14:38 +0000

    loo

--- src/app/add-post/page.jsx
+++ src/app/add-post/page.jsx
@@ -45,7 +45,7 @@ export default async function Home() {
           placeholder="Post title..."
           className="text-black px-3 py-2 rounded"
         />
-        <Tiptap
+        <textarea
           name="content"
           className="text-black px-3 py-2 rounded"
           placeholder="Post content"
--- src/components/Tiptap.jsx
+++ src/components/Tiptap.jsx
@@ -1,25 +1,25 @@
-"use client"
-import { useEditor, EditorContent } from "@tiptap/react";
-import StarterKit from "@tiptap/starter-kit";
+// "use client"
+// import { useEditor, EditorContent } from "@tiptap/react";
+// import StarterKit from "@tiptap/starter-kit";
 
 
-const Tiptap = ({ onChange, content }) => {
+// const Tiptap = ({ onChange, content }) => {
  
-  const editor = useEditor({
-    extensions: [StarterKit],
-    editorProps: {
-      attributes: {
-        class:
-          "flex flex-col px-4 py-3 justify-start border-b border-r border-l border-gray-700 text-gray-400 items-start w-full gap-3 font-medium text-[16px] pt-4 rounded-bl-md rounded-br-md outline-none",
-      },
-    },
-  });
+//   const editor = useEditor({
+//     extensions: [StarterKit],
+//     editorProps: {
+//       attributes: {
+//         class:
+//           "flex flex-col px-4 py-3 justify-start border-b border-r border-l border-gray-700 text-gray-400 items-start w-full gap-3 font-medium text-[16px] pt-4 rounded-bl-md rounded-br-md outline-none",
+//       },
+//     },
+//   });
 
-  return (
-    <div className="w-full px-4">
-      <EditorContent style={{ whiteSpace: "pre-line" }} editor={editor} />
-    </div>
-  );
-};
+//   return (
+//     <div className="w-full px-4">
+//       <EditorContent style={{ whiteSpace: "pre-line" }} editor={editor} />
+//     </div>
+//   );
+// };
 
-export default Tiptap;
+// export default Tiptap;
